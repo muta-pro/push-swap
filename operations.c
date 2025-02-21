8c7bd6db149e409f31f06596cf96d4c21cf2d447
Ivan Mutavdzic <imutavdz@f1r4s18.codam.nl>
2025-02-21 22:50:09 +0100

    add

--- operations.c
+++ operations.c
@@ -1,36 +1,20 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   h_p_s.h                                            :+:      :+:    :+:   */
+/*   operations.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: imutavdz <imutavdz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/02/21 21:29:57 by imutavdz          #+#    #+#             */
-/*   Updated: 2025/02/21 22:49:47 by imutavdz         ###   ########.fr       */
+/*   Created: 2025/02/21 21:53:06 by imutavdz          #+#    #+#             */
+/*   Updated: 2025/02/21 22:49:46 by imutavdz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIST_H
-#define LIST_H
-
-typedef struct s_node
+void pull_up(t_stack *stack)
 {
-	int value;
-	struct s_node *next;
-	struct s_node *prev;
-} t_node;
-
-typedef struct s_stack
-{
-	int *uppest;
-	int *lowest;
-	int size;
-	char a;
-} t_stack;
-
-#include <unistd.h>
-#include <stdio.h>
-#include "libft/libft.h"
-
-
-#endif
+	if (!stack->uppest || !stack->uppest->next)
+		return;
+	t_node *prev_up = stack->uppest;
+	t_node *next_up = stack->uppest->next;
+	t_node *prev_low = stack->lowest;	
+}
